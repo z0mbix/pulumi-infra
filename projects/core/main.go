b24341af69480a5830679029960fab4a160a2d73
z0mbix <zombie@zombix.org>
2023-07-23 16:03:55 +0100

    Add EC2 IAM policy in core for other projects to use and create simple instance in an existing VPC

--- projects/core/main.go
+++ projects/core/main.go
@@ -6,6 +6,7 @@ import (
 
 	"common"
 
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/iam"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
@@ -13,6 +14,8 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Get configuration values
+		// projectName := ctx.Project()
 		stackName := ctx.Stack()
 		cfg := config.New(ctx, "")
 
@@ -20,7 +23,15 @@ func main() {
 		var env common.Environment
 		cfg.RequireObject("environment", &env)
 
-		// Create the zombix infrastructure bucket
+		// Tags for all resources
+		// defaultTags := pulumi.StringMap{
+		// 	"EnvironmentName": pulumi.String(env.Name),
+		// 	"ManagedBy":       pulumi.String("Pulumi"),
+		// 	"PulumiProject":   pulumi.String(ctx.Project()),
+		// 	"PulumiStack":     pulumi.String(stackName),
+		// }
+
+		// Create the zinfrastructure bucket
 		infraBucketConfig := common.Bucket{
 			Prefix: fmt.Sprintf("%s-infra", common.BucketPrefix),
 		}
@@ -28,6 +39,7 @@ func main() {
 		cfg.RequireObject("infraBucket", &infraBucketConfig)
 		infraBucketConfig.Name = fmt.Sprintf("%s-%s", infraBucketConfig.Prefix, stackName)
 
+		// bucketTags :=
 		bucket, err := s3.NewBucket(ctx, "infrastructure-bucket", &s3.BucketArgs{
 			Bucket: pulumi.String(infraBucketConfig.Name),
 			Acl:    pulumi.String("private"),
@@ -41,7 +53,10 @@ func main() {
 			Tags: pulumi.StringMap{
 				"DataClassification": pulumi.String(infraBucketConfig.DataClassification),
 				"EnvironmentName":    pulumi.String(env.Name),
+				"ManagedBy":          pulumi.String("Pulumi"),
 				"Name":               pulumi.String(infraBucketConfig.Name),
+				"PulumiProject":      pulumi.String(ctx.Project()),
+				"PulumiStack":        pulumi.String(stackName),
 			},
 		})
 		if err != nil {
@@ -64,10 +79,50 @@ func main() {
 			return err
 		}
 
+		// Create an IAM policy allowing read access to the infrastructure bucket
+		policyName := fmt.Sprintf("%s-ec2-core", stackName)
+		policy, err := iam.NewPolicy(ctx, policyName, &iam.PolicyArgs{
+			Description: pulumi.String("Allows reading objects from the zombix-infra S3 bucket"),
+			Policy: pulumi.Sprintf(`{
+				"Version": "2012-10-17",
+				"Statement": [
+					{
+						"Effect": "Allow",
+						"Action": [
+							"s3:GetObject",
+							"s3:ListBucket"
+						],
+						"Resource": [
+							"arn:aws:s3:::%s",
+							"arn:aws:s3:::%s/*"
+						]
+					},
+					{
+						"Effect": "Allow",
+						"Action": [
+							"tag:GetResources"
+						],
+						"Resource": ["*"]
+					}
+				]
+			}`, bucket.ID(), bucket.ID()),
+			Tags: pulumi.StringMap{
+				"EnvironmentName": pulumi.String(env.Name),
+				"ManagedBy":       pulumi.String("Pulumi"),
+				"Name":            pulumi.String(policyName),
+				"PulumiProject":   pulumi.String(ctx.Project()),
+				"PulumiStack":     pulumi.String(stackName),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
 		// Exports
 		ctx.Export("bucketName", bucket.ID())
 		ctx.Export("bucketArn", bucket.Arn)
-		ctx.Export("userData", pulumi.Sprintf("s3://%s/%s", bucket.ID(), userData.Key))
+		ctx.Export("userDataUrl", pulumi.Sprintf("s3://%s/%s", bucket.ID(), userData.Key))
+		ctx.Export("ec2InstanceIamPolicy", policy.Arn)
 
 		return nil
 	})
--- projects/nodes/main.go
+++ projects/nodes/main.go
@@ -2,107 +2,60 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
+type Ami struct {
+	Distro string
+	Id     string
+	Owner  string
+	Name   string
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Get some configuration values or set default values.
+		// Get configuration values
+		projectName := ctx.Project()
+		stackName := ctx.Stack()
 		cfg := config.New(ctx, "")
 		instanceType := "t3.micro"
 		if param := cfg.Get("instanceType"); param != "" {
 			instanceType = param
 		}
-		vpcNetworkCidr := "10.0.0.0/16"
-		if param := cfg.Get("vpcNetworkCidr"); param != "" {
-			vpcNetworkCidr = param
-		}
-		
-		// Look up the latest Amazon Linux 2 AMI.
-		ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
+
+		vpcId := cfg.Require("vpcId")
+		subnetId := cfg.Require("subnetId")
+		sshKeypairName := cfg.Require("sshKeypairName")
+
+		// Lookup the latest AMI
+		var ami Ami
+		cfg.RequireObject("ami", &ami)
+
+		amiResult, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
 			Filters: []ec2.GetAmiFilter{
-				ec2.GetAmiFilter{
-					Name: "name",
-					Values: []string{
-						"amzn2-ami-hvm-*",
-					},
-				},
-			},
-			Owners: []string{
-				"amazon",
+				{Name: "name", Values: []string{ami.Name}},
 			},
 			MostRecent: pulumi.BoolRef(true),
+			Owners:     []string{ami.Owner},
 		}, nil)
 		if err != nil {
 			return err
 		}
+		ami.Id = amiResult.Id
 
-		// User data to start a HTTP server in the EC2 instance
-		userData := "#!/bin/bash\necho \"Hello, World from Pulumi!\" > index.html\nnohup python -m SimpleHTTPServer 80 &\n"
-		
-		// Create VPC.
-		vpc, err := ec2.NewVpc(ctx, "vpc", &ec2.VpcArgs{
-			CidrBlock:          pulumi.String(vpcNetworkCidr),
-			EnableDnsHostnames: pulumi.Bool(true),
-			EnableDnsSupport:   pulumi.Bool(true),
-		})
-		if err != nil {
-			return err
-		}
-
-		// Create an internet gateway.
-		gateway, err := ec2.NewInternetGateway(ctx, "gateway", &ec2.InternetGatewayArgs{
-			VpcId: vpc.ID(),
-		})
-		if err != nil {
-			return err
-		}
-		
-		// Create a subnet that automatically assigns new instances a public IP address.
-		subnet, err := ec2.NewSubnet(ctx, "subnet", &ec2.SubnetArgs{
-			VpcId:               vpc.ID(),
-			CidrBlock:           pulumi.String("10.0.1.0/24"),
-			MapPublicIpOnLaunch: pulumi.Bool(true),
-		})
-		if err != nil {
-			return err
-		}
-		
-		// Create a route table.
-		routeTable, err := ec2.NewRouteTable(ctx, "routeTable", &ec2.RouteTableArgs{
-			VpcId: vpc.ID(),
-			Routes: ec2.RouteTableRouteArray{
-				&ec2.RouteTableRouteArgs{
-					CidrBlock: pulumi.String("0.0.0.0/0"),
-					GatewayId: gateway.ID(),
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
-		
-		// Associate the route table with the public subnet.
-		_, err = ec2.NewRouteTableAssociation(ctx, "routeTableAssociation", &ec2.RouteTableAssociationArgs{
-			SubnetId:     subnet.ID(),
-			RouteTableId: routeTable.ID(),
-		})
-		if err != nil {
-			return err
-		}
-
-		// Create a security group allowing inbound access over port 80 and outbound
-		// access to anywhere.
-		secGroup, err := ec2.NewSecurityGroup(ctx, "secGroup", &ec2.SecurityGroupArgs{
-			Description: pulumi.String("Enable HTTP access"),
-			VpcId:       vpc.ID(),
+		// Security Group
+		securityGroup, err := ec2.NewSecurityGroup(ctx, fmt.Sprintf("nodes-%s", stackName), &ec2.SecurityGroupArgs{
+			Description: pulumi.String("Allow SSH inbound"),
+			VpcId:       pulumi.String(vpcId),
 			Ingress: ec2.SecurityGroupIngressArray{
 				&ec2.SecurityGroupIngressArgs{
-					FromPort: pulumi.Int(80),
-					ToPort:   pulumi.Int(80),
-					Protocol: pulumi.String("tcp"),
+					Description: pulumi.String("SSH"),
+					FromPort:    pulumi.Int(22),
+					ToPort:      pulumi.Int(22),
+					Protocol:    pulumi.String("tcp"),
 					CidrBlocks: pulumi.StringArray{
 						pulumi.String("0.0.0.0/0"),
 					},
@@ -110,30 +63,45 @@ func main() {
 			},
 			Egress: ec2.SecurityGroupEgressArray{
 				&ec2.SecurityGroupEgressArgs{
-					FromPort: pulumi.Int(0),
-					ToPort:   pulumi.Int(0),
-					Protocol: pulumi.String("-1"),
+					Description: pulumi.String("Allow all outbound traffic - Yᵒᵘ Oᶰˡʸ Lᶤᵛᵉ Oᶰᶜᵉ"),
+					FromPort:    pulumi.Int(0),
+					ToPort:      pulumi.Int(0),
+					Protocol:    pulumi.String("-1"),
 					CidrBlocks: pulumi.StringArray{
 						pulumi.String("0.0.0.0/0"),
 					},
 				},
+			},
+			Tags: pulumi.StringMap{
+				"Name":        pulumi.String("allow_ssh"),
+				"Environment": pulumi.String("dev"),
 			},
 		})
 		if err != nil {
 			return err
 		}
 
+		// User data to start a HTTP server in the EC2 instance
+		// userData := "#!/usr/bin/env bash\necho \"Hello, World from Pulumi!\" > index.html\nnohup python3 -m http.server 80 &\n"
+
+		coreStack, err := pulumi.NewStackReference(ctx, fmt.Sprintf("%s/core/%s", projectName, stackName), nil)
+		if err != nil {
+			return err
+		}
+		userData := coreStack.GetStringOutput(pulumi.String("userDataUrl"))
+
 		// Create and launch an EC2 instance into the public subnet.
 		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
+			Ami:          pulumi.String(ami.Id),
 			InstanceType: pulumi.String(instanceType),
-			SubnetId:     subnet.ID(),
-			VpcSecurityGroupIds: pulumi.StringArray{
-				secGroup.ID(),
-			},
-			UserData: pulumi.String(userData),
-			Ami:      pulumi.String(ami.Id),
+			KeyName:      pulumi.String(sshKeypairName),
+			SubnetId:     pulumi.String(subnetId),
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("webserver"),
+				"Name": pulumi.String("node"),
+			},
+			UserData: userData,
+			VpcSecurityGroupIds: pulumi.StringArray{
+				securityGroup.ID(),
 			},
 		})
 		if err != nil {
